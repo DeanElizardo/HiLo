7faf8c25e1d750d58bfcb33161e0c042043eac20
Dean Elizardo <del@delizardo.com>
2023-02-12 08:33:52 -0600

    :lipstick:

--- hilo-frontend/src/components/Card.jsx
+++ hilo-frontend/src/components/Card.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export function Card({card, onClick}) {
-  return <img src={`/assets/cards/${card}`} alt={"A playing card"} onClick={onClick}/>
+  return <img 
+    src={`/assets/cards/${card}`} 
+    alt={"A playing card"}
+    className="card" 
+    onClick={onClick}/>
 }
